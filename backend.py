2999ee003c0e60d05e09eb6e38918f5efdd33220
Sologene <basil.s.maliyil@gmail.com>
2024-05-28 11:51:30 +0800

    7/7/24: Added backend for sky and tempratures logic

--- backend.py
+++ backend.py
@@ -9,11 +9,6 @@ def get_data(place, days=None, kind=None):
     data = contents["list"]
     nr = 8 * days
     data = data[:nr]
-
-    if kind == "Temperature":
-        data = [dit["main"]["temp"] for dit in data]
-    if kind == "Sky":
-        data = [dit["weather"][0]["main"] for dit in data]
     return data
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -5,8 +5,24 @@ st.title("Weather forecast for the Next Days")
 place = st.text_input("Place: ")
 days = st.slider("Forecast days", min_value=1, max_value=5, help = "Select the number of forecasted days")
 option = st.selectbox("Select data to view",
-                      ("Temprature", "Sky"))
-data = get_data(place, days, option)
-st.subheader(f"{option} for the next {days} days in {place} ")
-figure = px.line(x, y,labels={"x":"Date", "y":"Temprature (C)"})
-st.plotly_chart(figure)
+                      ("Temperature", "Sky"))
+try:
+    if place:
+        st.subheader(f"{option} for the next {days} days in {place} ")
+        data = get_data(place, days)
+        if option == "Temperature":
+            temp = [dit["main"]["temp"] for dit in data]
+            temp = [x/10 for x in temp]
+            dates = [dit["dt_txt"] for dit in data]
+            figure = px.line(x=dates, y=temp,labels={"x":"Date", "y":"Temperature (C)"})
+            st.plotly_chart(figure)
+
+        if option == "Sky":
+            dates = [dit["dt_txt"] for dit in data]
+            images = {"Clear": "images/clear.png","Rain": "images/rain.png", "Clouds": "images/clouds.png", "Snow": "images/snow.png" }
+            sky = [dit["weather"][0]["main"] for dit in data]
+            img_path = [images[img] for img in sky]
+            st.image(img_path, width=200, caption=dates)
+except KeyError:
+    st.info("You entered a  non-existing place")
+
